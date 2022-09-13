6a29523ca06f3962cd3de7472dbe650c5fbf486e
MissMunawwarah <madigun39@gmail.com>
2022-09-13 13:19:55 -0700

    Oil

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,21 @@
 #include "main.h"
 
 /**
- * main - prints _putchar, followed by a new line.
+ * main - Entry point
  *
- * Return: Always 0.
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
 */
 
-int main()
+int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ++ch)
+		_putchar(str[ch]);
+	_putchar('\n');
 
 	return (0);
 }
