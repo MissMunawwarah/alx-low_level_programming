673e1de529f19853a1900dfa8c4b89cffcc80016
MissMunawwarah <madigun39@gmail.com>
2022-09-30 10:02:12 -0700

    oil

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * main - Print new name of the program
+ * main - Print the name of the program
  * @argc: Count arguments
  * @argv: Arguments
  * Return: Always 0 (Success)
